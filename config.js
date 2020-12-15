0b3191bbea1219bea504f9b396f95175a12b7b45
carage34 <jully.morgan@orange.fr>
2020-12-15 02:09:34 +0100

    add dest id in config file

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ dotenv.config();
 module.exports ={
     verify_token: process.env.VERIFY_TOKEN,
     access_token: process.env.ACCESS_TOKEN,
-    port: process.env.PORT
+    port: process.env.PORT,
+    server_address: process.env.SERVER_ADDRESS,
+    server_port: process.env.SERVER_PORT,
+    dest_id: process.env.DEST_ID
 }
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-bodyParser = require('body-parser');
-express = require('express');
+const bodyParser = require('body-parser');
+const express = require('express');
+const redis = require("redis");
 const message = require("./message");
 const hltvparse = require("./hltv-parse");
-const { port, verify_token } = require('./config');
+const { server_address, server_port, dest_id } = require('./config');
 
 const BASE_HLTV_URL = "https://www.hltv.org/team/";
 const VITALITY = "9565/vitality#tab-matchesBox";
 const G2 = "5995/g2#tab-matchesBox"
 const VITALITY_NAME = "Vitality"
 const G2_NAME = "G2";
-const DEST_ID = 3557668797646399
+const DEST_ID = dest_id;
 
-hltvparse.getNexMatch(BASE_HLTV_URL + VITALITY, VITALITY_NAME).then(function(msg) {
+let client = redis.createClient({
+    port: server_port,
+    host: server_address
+});
+
+client.on('error', function(error) {
+    console.log(error);
+});
+
+//client.set("key", "value", redis.print);
+client.get("key", redis.print);
+
+/*hltvparse.getNexMatch(BASE_HLTV_URL + VITALITY, VITALITY_NAME).then(function(msg) {
     message.callSendApi(DEST_ID, msg);
-})
-
+})*/
